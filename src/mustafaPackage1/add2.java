a8c4cb656caa6974a77c27af7ebdbb8183401d25
Mustafa <mustafamuezzinoglu@gmail.com>
2023-02-12 22:23:24 +0300

    Assignee try

--- src/mustafaPackage1/add2.java
+++ src/mustafaPackage1/add2.java
@@ -2,5 +2,11 @@ package mustafaPackage1;
 
 public class add2{
 //    push back!
+public static void main(String[] args) {
+    int a=10;
+    int b=20;
+    int c=a+b;
+    System.out.println(c);
+}
 //   pull 
 }
